6d5333158a63cd857c2d67d06e72ee581b6179bc
vinicius Leoni <viniciuslg@dcc.ufrj.br>
2024-11-21 00:33:25 -0300

    Update Teste_Display_LCD_com_modulo_I2C.ino

--- Teste_Display_LCD_com_modulo_I2C/Teste_Display_LCD_com_modulo_I2C.ino
+++ Teste_Display_LCD_com_modulo_I2C/Teste_Display_LCD_com_modulo_I2C.ino
@@ -1,24 +1,16 @@
-//YWROBOT
-//Compatible with the Arduino IDE 1.0
-//Library version:1.1
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-//porta A4 para o SDA e porta A5 para o SCL
-void setup()
-{
-  lcd.init();                      // initialize the lcd 
-  lcd.init();
-  // Print a message to the LCD.
+LiquidCrystal_I2C lcd(0x27,16,2); // set the LCD address to 0x27 for a 16 chars and 2 line display
+                                  //porta A4 para o SDA e porta A5 para o SCL
+void setup(){
+  lcd.init(); 
   lcd.backlight();
-
 }
 
 
-void loop()
-{
-    lcd.setCursor(3,0);
+void loop(){
+  lcd.setCursor(3,0);             //Escolhe a posição do caractere que começará a ser escrita a frase, display tem 2 linhas com 16 slots de caractere cada
   lcd.print("Blink Display");
   delay(1000);
   lcd.clear();
