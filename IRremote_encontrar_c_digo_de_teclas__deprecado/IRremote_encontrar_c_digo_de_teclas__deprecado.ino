e8672b3583fe22a9346d2bcd7c4b4367c82ff818
vinicius Leoni <viniciuslg@dcc.ufrj.br>
2024-11-21 00:29:07 -0300

    Update IRremote_encontrar_c_digo_de_teclas__deprecado.ino

--- IRremote_encontrar_c_digo_de_teclas__deprecado/IRremote_encontrar_c_digo_de_teclas__deprecado.ino
+++ IRremote_encontrar_c_digo_de_teclas__deprecado/IRremote_encontrar_c_digo_de_teclas__deprecado.ino
@@ -6,7 +6,6 @@ decode_results results;
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   irrecv.enableIRIn();
   irrecv.blink13(true);
@@ -14,7 +13,6 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   if(irrecv.decode(&results)){
     Serial.println(results.value, HEX);
     irrecv.resume();
