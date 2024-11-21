b6af293511b181fca97af0386ca045ae8910f0d3
vinicius Leoni <viniciuslg@dcc.ufrj.br>
2024-11-21 00:20:39 -0300

    Update buzzer_e_ultrassom.ino

--- buzzer_e_ultrassom/buzzer_e_ultrassom.ino
+++ buzzer_e_ultrassom/buzzer_e_ultrassom.ino
@@ -1,38 +1,34 @@
-// Defina os pinos de conexão do sensor
-int trigPin = 10;  // Pino TRIG do sensor
-int echoPin = 11; // Pino ECHO do sensor
+int trigPin = 10;  
+int echoPin = 11; 
 int buzzer = 2;
-// Variável para armazenar a distância
+
 
 long getDistance(int trigger, int echo){
   // Função que calcula a distância entre o sensor HC-SR04 e um sólido próximo dele 
   long duration;
   int distance;
 
-  // Gera um pulso de 10µs no pino trigger
-  // O sensor HC-SR04 está projetado para ser acionado com um pulso de exatamente 10 microssegundos (µs) no pino TRIG
+  // Gera pulso de 10µs no pino trigger
+  // O HC-SR04 é projetado para ser acionado com um pulso de exatamente 10 microssegundos (µs) no pino TRIG
   digitalWrite(trigger, LOW);
   delayMicroseconds(2);
   digitalWrite(trigger, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigger, LOW);
-  // Lê o tempo que o pulso leva para retornar
   duration = pulseIn(echoPin, HIGH);
-  // Calcula a distância em centímetros
-  distance = duration * 0.034 / 2; // Velocidade do som = 340 m/s ou 0.034 cm/µs // Divisão por 2 pois a duração tem ida e volta
+  // Distância em centímetros
+  distance = duration * 0.034 / 2; // Fórmula: Velocidade do som = 340 m/s ou 0.034 cm/µs // Divisão por 2 pois a duração tem ida e volta
   return distance;
   }
 
 void setup() {
   Serial.begin(9600);
   // Configura os pinos TRIG e ECHO
-  pinMode(trigPin, OUTPUT); //saída
-  pinMode(echoPin, INPUT);  //entrada
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT); 
 }
 
 void loop() {
-
-  // Exibe a distância no monitor serial
   long dist = getDistance(trigPin,echoPin);
   Serial.print("Distância: ");
   Serial.print(dist);
@@ -42,11 +38,8 @@ void loop() {
     tone(buzzer,2000);
     delay(500);
     tone(buzzer,500);
-    
   }
   else{
     noTone(buzzer);
     }
-  
-
 }
